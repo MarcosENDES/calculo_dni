cf3db14085ddb52bf9d391bd9a44bf7ec13984a1
OEM Configuration (temporary user) <rios.garcia.marcos@iescamas.es>
2025-01-14 12:21:39 +0100

    ultimos test

--- calculo-dni/src/test/java/com/endes/ControladorDNITest.java
+++ calculo-dni/src/test/java/com/endes/ControladorDNITest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.Test;
  */
 class ControladorDNITest {
 
+	ControladorDNI controlador;
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -30,17 +32,40 @@ class ControladorDNITest {
 	@BeforeEach
 	void setUp() throws Exception {
 		//.out.println("@BeforeEach");
+		controlador = new ControladorDNI();
 	}
 	
 	// Prueba para verificar que el calculo de la letra del DNI es correcto
 	@Test
-	void testCalcularLetraValida() {
+	void testEsValidoDNI() {
 		// Caso de prueba válido
-		String numeroDNI = "11111111";
-		char letraEsperada = 'H';
-		
-		
+		String dniCorrecto = "11111111H";
+		assertTrue(controlador.esValido(dniCorrecto));
 	}
 
+	@Test
+	@DisplayName("Validacion incorrecta")
+	void testEsFalsoDNI() {
+		String dniFalso = "11111111R";
+		
+		assertFalse(controlador.esValido(dniFalso));
+	}
 
+	@Test
+	@DisplayName("Validacion de entradas validas para DNI")
+	void testEsValido_DNIEntradasInvalidas() {
+		assertFalse(controlador.esValido(null), "Un DNI nulo fue reconocido como valido");
+	}
+	
+	@Test
+	@DisplayName("Un dni demasiado corto")
+	void testDNIcorto() {
+		assertFalse(controlador.esValido("11111111H"), "El dni es demasiado corto y fue");
+	}
+	
+	@Test
+	@DisplayName("DNI con caracteres no numéricos")
+	void testDNIconCaracteres() {
+		
+	}
 }
